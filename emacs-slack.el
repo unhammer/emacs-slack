8d83ee62faffd079ccb5911d527020898da2fff9
Andrea <andrea-dev@hotmail.com>
2024-11-19 10:30:46 +0000

    add dependencies to slack-pkg.el
    fixes https://github.com/emacs-slack/emacs-slack/issues/592

--- emacs-slack.el
+++ emacs-slack.el
@@ -9,7 +9,6 @@
 ;;   Email: andrea-dev@hotmail.com
 ;; Keywords: tools
 ;; Version: 0.0.3
-;; Package-Requires: ((websocket "1.12") (request "0.3.2") (circe "2.11") (alert "1.2") (emojify "1.2.1") (emacs "25.1") (dash "2.19.1") (s "1.13.1"))
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
--- slack-pkg.el
+++ slack-pkg.el
@@ -2,8 +2,11 @@
   "Slack client for Emacs"
   '((websocket "1.8")
     (request "0.2.0")
-    (oauth2 "0.10")
+    (oauth2 "0.10") ;; TODO not sure this is really needed
     (circe "2.2")
     (alert "1.2")
-    (emojify "0.2"))
+    (emojify "0.2")
+    (emacs "25.1")
+    (dash "2.19.1")
+    (s "1.13.1"))
   :url "https://github.com/emacs-slack/emacs-slack")
